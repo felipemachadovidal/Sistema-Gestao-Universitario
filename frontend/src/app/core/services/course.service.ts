f40504d54f71f77f307a7050d822b808afffb771
Felipe <lipem61@gmail.com>
2026-06-21 14:32:16 -0300

    feat: course frontend add

--- frontend/src/app/core/services/course.service.ts
+++ frontend/src/app/core/services/course.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface CourseRequest {
+  name: string;
+  description: string;
+  durationHours: number;
+}
+
+export interface CourseResponse {
+  id: number;
+  name: string;
+  description: string;
+  durationHours: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  private readonly apiUrl = 'http://localhost:8081/api/courses';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  listAll(): Observable<CourseResponse[]> {
+    return this.http.get<CourseResponse[]>(this.apiUrl);
+  }
+
+  create(course: CourseRequest): Observable<CourseResponse> {
+    return this.http.post<CourseResponse>(this.apiUrl, course);
+  }
+
+  softDelete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- frontend/src/app/pages/course-list/course-list.component.ts
+++ frontend/src/app/pages/course-list/course-list.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseService, CourseResponse } from '../../core/services/course.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+  private courseService = inject(CourseService);
 
+  courses = signal<CourseResponse[]>([]);
+  isLoading = signal<boolean>(true);
+
+  ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses(): void {
+    this.isLoading.set(true);
+    this.courseService.listAll().subscribe({
+      next: (data) => {
+        this.courses.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.isLoading.set(false);
+      }
+    });
+  }
+
+  deleteCourse(id: number): void {
+    if (confirm('Tem certeza que deseja remover este curso?')) {
+      this.courseService.softDelete(id).subscribe({
+        next: () => {
+          this.courses.update(all => all.filter(c => c.id !== id));
+        }
+      });
+    }
+  }
 }
